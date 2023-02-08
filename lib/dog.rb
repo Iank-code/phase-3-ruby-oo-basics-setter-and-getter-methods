3aae20e480171211dea4cbaf6980fb88e89020d5
ianK-code <kian99564@gmail.com>
2023-02-08 14:51:48 +0300

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    # Setters
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
 
+    # Getters of you can use: attr_reader :name, :breed
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
 end
